8e162b7e03e7fa7d92b816c52393c9a16d8c9a94
TroddenSpade <parsasam1378@gmail.com>
2019-10-04 12:54:56 +0330

    home and input screen added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,55 @@
 import React from "react";
-import { Text, TouchableOpacity, SafeAreaView, StatusBar } from "react-native";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet
+} from "react-native";
 
 export default class Home extends React.Component {
   render() {
     return (
       <>
         <StatusBar barStyle="dark-content" />
-        <SafeAreaView>
-          <TouchableOpacity
-            onPress={() => this.props.navigation.navigate("Input")}
-          >
-            <Text>Start</Text>
-          </TouchableOpacity>
+        <SafeAreaView style={styles.container}>
+          <View style={styles.top}>
+            <Text>Nights Fall In Palermo</Text>
+          </View>
+          <View style={styles.bottom}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => this.props.navigation.navigate("Input")}
+            >
+              <Text>Lets Shed Some Blood</Text>
+            </TouchableOpacity>
+          </View>
         </SafeAreaView>
       </>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+    // backgroundColor: "black"
+  },
+  top: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  bottom: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  button: {
+    borderColor: "grey",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10
+  }
+});
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,144 @@
-import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import React from 'react';
+import {
+  View,
+  Text,
+  Picker,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+  SafeAreaView,
+  Button,
+} from 'react-native';
 
-export default Input = () => {
-  return (
-    <View>
-      <Text>HI</Text>
-      <TouchableOpacity onPress={() => this.props.navigation.navigate("Input")}>
-        <Text>Start</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
+export default class Input extends React.Component {
+  state = {
+    no: 5,
+    names: [],
+  };
+
+  playerNo = [
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31,
+    32,
+    33,
+    34,
+    35,
+    36,
+    37,
+    38,
+    39,
+    40,
+    41,
+    42,
+    43,
+    44,
+    45,
+  ];
+
+  done() {
+    for (let i = 0; i < this.state.no; i++) {
+      if (this.state.names[i] === '' || this.state.names[i] === undefined) {
+        alert('empty');
+        return;
+      }
+    }
+    alert('nice');
+  }
+
+  render() {
+    let list = [];
+    for (let i = 0; i < this.state.no; i++) {
+      list.push(
+        <TextInput
+          style={styles.textinput}
+          onChangeText={text => {
+            let names = this.state.names.slice();
+            names[i] = text;
+            this.setState({
+              names,
+            });
+          }}
+          value={this.state.names[i]}
+        />,
+      );
+    }
+    return (
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <View style={styles.body}>
+            <View style={styles.top}>
+              <View>
+                <Text>Number of Players</Text>
+              </View>
+              <Picker
+                style={styles.picker}
+                itemStyle={{height: 100}}
+                selectedValue={this.state.no}
+                onValueChange={newNo => this.setState({no: newNo})}>
+                {this.playerNo.map(item => (
+                  <Picker.Item label={item.toString()} value={item} />
+                ))}
+              </Picker>
+            </View>
+            <View style={styles.users}>{list}</View>
+          </View>
+        </ScrollView>
+        <Button title="Pick up Roles" onPress={this.done.bind(this)} />
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  top: {
+    backgroundColor: 'red',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  picker: {
+    width: 50,
+  },
+  users: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  scrollView: {
+    height: '90%',
+  },
+  textinput: {
+    margin: 10,
+    width: 200,
+    height: 50,
+    borderColor: 'lightgrey',
+    borderWidth: 1,
+    padding: 10,
+  },
+});
